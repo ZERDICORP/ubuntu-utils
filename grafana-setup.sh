836562b582333a7ad41a1a3c420351b59da4e25c
Nikolay Anikin <56264511+ZERDICORP@users.noreply.github.com>
2024-10-14 03:11:56 +0300

    Update grafana-setup.sh

--- grafana-setup.sh
+++ grafana-setup.sh
@@ -1,14 +1,17 @@
 sudo apt-get install -y apt-transport-https software-properties-common wget
 
 sudo mkdir -p /etc/apt/keyrings/
+
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install grafana -y
+sudo apt install grafana -y
 
-sudo systemctl start grafana-server.service
+sudo systemctl enable grafana
+
+sudo systemctl start grafana
